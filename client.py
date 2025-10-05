ed104e227ca4505ff86f638cae1bf53786f16f4f
Prodyot Sarkar <prodyotsarkar@Prodyots-MacBook-Air.local>
2025-10-05 12:19:45 -0700

    adding API setup

---
+++ client.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+# local API
+url = "http://localhost:8005/medical_assistance/"
+
+question = {
+    "question": "What is seasonal allergy and what are the treatments available?"
+}
+
+# Send POST request
+response = requests.post(url, json=question)
+
+# Print response
+if response.status_code == 200:
+    print((response.json()['answer']))
+else:
+    print(response.status_code, response.text, response.reason,response,response.content)
